4a21be8307ecd2f2088cdd71dcff1bfae5cab7b5
Ryan <rnleach@users.noreply.github.com>
2019-02-05 21:22:16 -0700

    Added methods to get long and double vals.

--- src/error.rs
+++ src/error.rs
@@ -7,17 +7,19 @@ use std::{error::Error, ffi::CStr, fmt::Display};
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum BufrErr {
     CodesError(libc::c_int),
+    KeyNulError,
     NullPtr,
 }
 
 impl Display for BufrErr {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         match *self {
-            BufrErr::NullPtr => write!(f, "null pointer encountered."),
             BufrErr::CodesError(val) => unsafe {
                 let msg: &CStr = &CStr::from_ptr(codes_get_error_message(val));
                 write!(f, "{}", msg.to_string_lossy())
             },
+            BufrErr::KeyNulError => write!(f, "null value in the middle of a key"),
+            BufrErr::NullPtr => write!(f, "null pointer encountered."),
         }
     }
 }
@@ -27,6 +29,12 @@ impl Error for BufrErr {}
 impl From<libc::c_int> for BufrErr {
     fn from(val: libc::c_int) -> Self {
         BufrErr::CodesError(val)
+    }
+}
+
+impl From<std::ffi::NulError> for BufrErr {
+    fn from(_val: std::ffi::NulError) -> Self {
+        BufrErr::KeyNulError
     }
 }
 
--- src/message.rs
+++ src/message.rs
@@ -1,9 +1,13 @@
 use crate::error::BufrErr;
 use eccodes_sys::{
-    codes_get_error_message, codes_handle, codes_handle_delete, codes_set_long, CODES_SUCCESS,
+    codes_get_double, codes_get_error_message, codes_get_long, codes_handle, codes_handle_delete,
+    codes_set_long, CODES_SUCCESS,
 };
 use libc;
-use std::{borrow::Cow, ffi::CStr};
+use std::{
+    borrow::Cow,
+    ffi::{CStr, CString},
+};
 
 /// A single message from within a data source containing bufr data.
 pub struct Message {
@@ -14,6 +18,30 @@ impl Message {
     /// Get an iterator over the keys available in this message.
     pub fn keys(&self) -> Result<impl Iterator<Item = Cow<str>>, BufrErr> {
         keys::KeysIterator::new(self)
+    }
+
+    /// Retrieve a long value from the message.
+    pub fn long<K: Into<Vec<u8>>>(&self, key: K) -> Result<i64, BufrErr> {
+        let key: CString = CString::new(key)?;
+        let mut val = 0i64;
+
+        unsafe {
+            codes_check!(codes_get_long(self.handle, key.as_ptr(), &mut val))?;
+        }
+
+        Ok(val)
+    }
+
+    /// Retrieve a double value from the message.
+    pub fn double<K: Into<Vec<u8>>>(&self, key: K) -> Result<f64, BufrErr> {
+        let key: CString = CString::new(key)?;
+        let mut val = 0f64;
+
+        unsafe {
+            codes_check!(codes_get_double(self.handle, key.as_ptr(), &mut val))?;
+        }
+
+        Ok(val)
     }
 
     // Create a new message.
